67f65dd136ee1ee84e179ec08f90a66bcb774c10
Ariel Barmat <abarmat@gmail.com>
2021-07-31 18:30:10 -0300

    Add delegates management commands for the transaction-service

--- safe_cli/api/gnosis_transaction.py
+++ safe_cli/api/gnosis_transaction.py
@@ -1,8 +1,11 @@
 from typing import Any, Dict, List, Optional
 from urllib.parse import urljoin
+from web3 import Web3
 
+import time
 import requests
 
+from eth_account.signers.local import LocalAccount
 from gnosis.eth.ethereum_client import EthereumNetwork
 from gnosis.safe import SafeTx
 
@@ -21,6 +24,12 @@ class TransactionService(BaseAPI):
         EthereumNetwork.ARBITRUM: 'https://safe-transaction.arbitrum.gnosis.io',
         EthereumNetwork.BINANCE: 'https://safe-transaction.bsc.gnosis.io',
     }
+
+    @classmethod
+    def create_delegate_message_hash(cls, delegate_address: str) -> str:
+        totp = int(time.time()) // 3600
+        hash_to_sign = Web3.keccak(text=delegate_address + str(totp))
+        return hash_to_sign
 
     def data_decoded_to_text(self, data_decoded: Dict[str, Any]) -> Optional[str]:
         """
@@ -55,11 +64,41 @@ class TransactionService(BaseAPI):
             return response.json()
 
     def get_transactions(self, safe_address: str) -> List[Dict[str, Any]]:
-        response = self._get_request(f'/api/v1/safes/{safe_address}/multisig-transactions/')
+        response = TransactionService._get_request(f'/api/v1/safes/{safe_address}/multisig-transactions/')
         if not response.ok:
             raise BaseAPI(f'Cannot get transactions: {response.content}')
         else:
             return response.json().get('results', [])
+
+    def get_delegates(self, safe_address: str) -> List[Dict[str, Any]]:
+        response = self._get_request(f'/api/v1/safes/{safe_address}/delegates/')
+        if not response.ok:
+            raise BaseAPI(f'Cannot get delegates: {response.content}')
+        else:
+            return response.json().get('results', [])
+
+    def add_delegate(self, safe_address: str, delegate_address: str, label: str, signer_account: LocalAccount):
+        hash_to_sign = TransactionService.create_delegate_message_hash(delegate_address)
+        signature = signer_account.signHash(hash_to_sign)
+        add_payload = {
+            'safe': safe_address,
+            'delegate': delegate_address,
+            'signature': signature.signature.hex(),
+            'label': label
+        }
+        response = self._post_request(f'/api/v1/safes/{safe_address}/delegates/', add_payload)
+        if not response.ok:
+            raise BaseAPI(f'Cannot add delegate: {response.content}')
+
+    def remove_delegate(self, safe_address: str, delegate_address: str, signer_account: LocalAccount):
+        hash_to_sign = TransactionService.create_delegate_message_hash(delegate_address)
+        signature = signer_account.signHash(hash_to_sign)
+        remove_payload = {
+            'signature': signature.signature.hex()
+        }
+        response = self._delete_request(f'/api/v1/safes/{safe_address}/delegates/{delegate_address}/', remove_payload)
+        if not response.ok:
+            raise BaseAPI(f'Cannot remove delegate: {response.content}')
 
     def post_transaction(self, safe_address: str, safe_tx: SafeTx):
         url = urljoin(self.base_url, f'/api/v1/safes/{safe_address}/multisig-transactions/')
