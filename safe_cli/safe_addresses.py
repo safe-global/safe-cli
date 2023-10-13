04856016f2bc2515fda4c21f619b3bdbc3e850d7
Uxio Fuentefria <Uxio0@users.noreply.github.com>
2023-10-13 13:30:26 +0200

    Add zksync addresses

--- safe_cli/safe_addresses.py
+++ safe_cli/safe_addresses.py
@@ -42,7 +42,7 @@ def get_safe_l2_contract_address(ethereum_client: EthereumClient) -> ChecksumAdd
             "0x29fcB43b46531BcA003ddC8FCB67FFE91900C762",  # v1.4.1
             "0x3E5c63644E683549055b9Be8653de26E0B4CD36E",  # v1.3.0
             "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",  # v1.3.0
-            "0xfb1bffC9d739B8D520DaF37dF666da4C687191EA",  # v1.3.0 zkSync
+            "0x1727c2c531cf966f902E5927b98490fDFb3b2b70",  # v1.3.0 zkSync
         ],
     )
 
@@ -56,6 +56,7 @@ def get_default_fallback_handler_address(
             "0xfd0732Dc9E303f09fCEf3a7388Ad10A83459Ec99",  # v1.4.1
             "0xf48f2B2d2a534e402487b3ee7C18c33Aec0Fe5e4",  # v1.3.0
             "0x017062a1dE2FE6b99BE3d9d37841FeD19F573804",  # v1.3.0
+            "0x2f870a80647BbC554F3a0EBD093f11B4d2a7492A",  # v1.3.0 zkSync
         ],
     )
 
@@ -67,6 +68,7 @@ def get_proxy_factory_address(ethereum_client: EthereumClient) -> ChecksumAddres
             "0x4e1DCf7AD4e460CfD30791CCC4F9c8a4f820ec67",  # v1.4.1
             "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2",  # v1.3.0
             "0xC22834581EbC8527d974F8a1c97E1bEA4EF910BC",  # v1.3.0
+            "0xDAec33641865E4651fB43181C6DB6f7232Ee91c2",  # v1.3.0 zkSync
         ],
     )
 
@@ -78,6 +80,7 @@ def get_last_multisend_address(ethereum_client: EthereumClient) -> ChecksumAddre
             "0x38869bf66a61cF6bDB996A6aE40D5853Fd43B526",  # v1.4.1
             "0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761",  # v1.3.0
             "0x998739BFdAAdde7C933B942a68053933098f9EDa",  # v1.3.0
+            "0x0dFcccB95225ffB03c6FBB2559B530C2B7C8A912",  # v1.3.0 zkSync
         ],
     )
 
@@ -91,5 +94,6 @@ def get_last_multisend_call_only_address(
             "0x40A2aCCbd92BCA938b02010E17A5b8929b49130D",  # v1.4.1
             "0x40A2aCCbd92BCA938b02010E17A5b8929b49130D",  # v1.3.0
             "0xA1dabEF33b3B82c7814B6D82A79e50F4AC44102B",  # v1.3.0
+            "0xf220D3b4DFb23C4ade8C88E526C1353AbAcbC38F",  # v1.3.0 zkSync
         ],
     )
