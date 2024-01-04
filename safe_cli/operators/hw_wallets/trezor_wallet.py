22e9b872405fb2b86b48f7aeb759a5d07b7c964f
moisses89 <7888669+moisses89@users.noreply.github.com>
2024-01-04 23:05:39 +0100

    Support execute transaction from Trezor

--- safe_cli/operators/hw_wallets/trezor_wallet.py
+++ safe_cli/operators/hw_wallets/trezor_wallet.py
@@ -1,9 +1,16 @@
 from functools import lru_cache
 
+import rlp
 from eth_typing import ChecksumAddress, HexStr
+from hexbytes import HexBytes
 from trezorlib import tools
 from trezorlib.client import TrezorClient, get_default_client
-from trezorlib.ethereum import get_address, sign_typed_data_hash
+from trezorlib.ethereum import (
+    get_address,
+    sign_tx,
+    sign_tx_eip1559,
+    sign_typed_data_hash,
+)
 from trezorlib.ui import ClickUI
 from web3.types import TxParams
 
@@ -51,11 +58,77 @@ class TrezorWallet(HwWallet):
         )
         return signed.signature
 
-    def get_signed_raw_transaction(self, tx_parameters: TxParams) -> HexStr:
+    @raise_trezor_exception_as_hw_wallet_exception
+    def get_signed_raw_transaction(
+        self, tx_parameters: TxParams, chain_id: int
+    ) -> HexStr:
         """
 
         :param chain_id:
         :param tx_parameters:
         :return: raw transaction signed
         """
-        raise NotImplementedError
+        address_n = tools.parse_path(self.derivation_path)
+        if tx_parameters.get("maxPriorityFeePerGas"):
+            # EIP1559
+            v, r, s = sign_tx_eip1559(
+                self.client,
+                n=address_n,
+                nonce=tx_parameters["nonce"],
+                gas_limit=tx_parameters["gas"],
+                to=tx_parameters["to"],
+                value=tx_parameters["value"],
+                data=HexBytes(tx_parameters["data"]),
+                chain_id=chain_id,
+                max_gas_fee=tx_parameters.get("maxFeePerGas"),
+                max_priority_fee=tx_parameters.get("maxPriorityFeePerGas"),
+            )
+
+            encoded_transaction = (
+                "0x02"
+                + rlp.encode(
+                    [
+                        chain_id,
+                        tx_parameters["nonce"],
+                        tx_parameters.get("maxPriorityFeePerGas"),
+                        tx_parameters.get("maxFeePerGas"),
+                        tx_parameters["gas"],
+                        HexBytes(tx_parameters["to"]),
+                        tx_parameters["value"],
+                        HexBytes(tx_parameters["data"]),
+                        [],
+                        v,
+                        HexBytes(r),
+                        HexBytes(s),
+                    ]
+                ).hex()
+            )
+        else:
+            # Legacy transaction
+            v, r, s = sign_tx(
+                self.client,
+                n=address_n,
+                nonce=tx_parameters["nonce"],
+                gas_price=tx_parameters["gasPrice"],
+                gas_limit=tx_parameters["gas"],
+                to=tx_parameters["to"],
+                value=tx_parameters["value"],
+                data=HexBytes(tx_parameters.get("data")),
+                chain_id=chain_id,
+            )
+
+            encoded_transaction = rlp.encode(
+                [
+                    tx_parameters["nonce"],
+                    tx_parameters["gasPrice"],
+                    tx_parameters["gas"],
+                    HexBytes(tx_parameters["to"]),
+                    tx_parameters["value"],
+                    HexBytes(tx_parameters["data"]),
+                    v,
+                    HexBytes(r),
+                    HexBytes(s),
+                ]
+            ).hex()
+
+        return HexStr(encoded_transaction)
