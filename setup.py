01e0b229bb7eac9686aeffe5445985cedb8f9b33
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2024-03-18 16:13:43 +0100

    Support Python 3.12
    - Stop supporting Python 3.8

--- setup.py
+++ setup.py
@@ -37,12 +37,12 @@ setuptools.setup(
     },
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
 )
