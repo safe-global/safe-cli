6e18fe6a867f6f9c5dcee3f4223b0b28f4dfd69b
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2024-06-11 12:07:30 +0200

    Fix module export

--- src/safe_cli/operators/__init__.py
+++ src/safe_cli/operators/__init__.py
@@ -1,5 +1,11 @@
-# flake8: noqa F401
 from .enums import SafeOperatorMode
 from .exceptions import SafeServiceNotAvailable
 from .safe_operator import SafeOperator
 from .safe_tx_service_operator import SafeTxServiceOperator
+
+__all__ = [
+    'SafeOperator',
+    'SafeOperatorMode',
+    'SafeServiceNotAvailable',
+    'SafeTxServiceOperator'
+]
